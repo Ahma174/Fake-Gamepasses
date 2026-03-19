7cd2c9c44e3f4969c35e5cdc4bf6ffe39f9ef3f4
Slicer <ahamadabdi878@gmail.com>
2026-03-20 00:34:59 +0300

    Add V6 Loader to load external scripts

---
+++ V6 Loader.lua
@@ -0,0 +1,7 @@
+pcall(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ahma174/Fake-Gamepasses/refs/heads/main/V6/V6_1.lua"))()
+end)
+
+pcall(function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ahma174/Fake-Gamepasses/refs/heads/main/V6/V6_2.lua"))()
+end)
